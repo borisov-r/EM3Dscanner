5e3af73dafb4cb2e148b21f1098468fc33749e8d
Radoslav Borisov <radoslav.borisov@gmail.com>
2012-06-15 18:28:14 +0300

    Started refactoring and structure change.

---
+++ src/ErrorMessage.py
@@ -0,0 +1,37 @@
+r"""ErrorMessage module.
+
+This class is part of 3DEMscanner suit.
+
+Example:
+
+>>> ErrorMessage.fromInit.show()
+>>> ErrorMessage.fromReprap.show()
+>>> ErrorMessage.fromPNA.show()
+
+ErrorMessage class is the main class for three subclases:
+
+    fromInit
+    fromReprap
+    fromPNA
+
+To do:
+    -
+    -
+    -
+
+"""
+
+
+class ErrorMessage:
+
+    """ErrorMessage interface class.
+
+    An instace of this class represnts a data that can be stored and shown
+    when an error ocures in 3DEMscanner program suit.
+
+    This class will be more general that the three other classes, which
+    will contain the methods to add, change and remove messages.
+
+    """
+
+    pass
