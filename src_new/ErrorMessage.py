773c401c78c4d8e7c9856e7edcdae89afdf3a063
Radoslav Borisov <radoslav.borisov@gmail.com>
2012-06-29 11:18:32 +0300

    Added Paraview python presentation.

--- src_new/ErrorMessage.py
+++ src_new/ErrorMessage.py
@@ -21,6 +21,8 @@ To do:
 
 """
 
+import logging
+
 
 class ErrorMessage:
 
@@ -35,3 +37,11 @@ class ErrorMessage:
     """
 
     pass
+
+logging.warning('Watch out!')  # will print a message to the console
+logging.info('I told you so')  # will not print anything
+
+logging.basicConfig(filename='example.log', level=logging.DEBUG)
+logging.debug('This message should go to the log file')
+logging.info('So should this')
+logging.warning('And this, too')
