4b70f2d01216ddf453b1e5909bc54606f483a55b
Radoslav Borisov <radoslav.borisov@gmail.com>
2013-08-12 18:36:09 +0300

    Started console based EM3Dscanner for basic usage.

---
+++ src_terminal/EM3DrfAtmega128.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+#==============================================================================
+# 3DEMnalib.py is part of 3DEMscanner suit software.
+#
+# 3DEMscanner is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ParaScan is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Printrun. If not, see <http://www.gnu.org/licenses/>
+#==============================================================================
+
+""" Filename: EM3Dnalib.py
+
+3DEMscanner is a macro for ParaView for automation of measurement process of EM
+waves with RepRap, Probe, PNA and ParaView
+The scanner consists of:
+
+    * Probe     - different probes are used for E and H vector of EM field
+    * RepRap    - control the movement of the 'Probe'
+    * PNA       - measurements using Agilent N5230C Network Analyzer
+    * ParaView  - data visualization software
+
+Official web site: https://github.com/borisov-r/3DEMscanner/wiki
+
+    nalib  is the basic library used to communicate with Agilent N5230C PNA.
+
+Main class is called 'NetworkAnalyzer' and consists of following methods:
+    - __init__("10.1.15.106", "5024")
+    - connect()     # return True on success
+    - disconnect()  # return True on success
+    - send('message')
+    - receive()
+    - receiveError()    # check if Error in PNA buffer
+"""
+import sys
+sys.path.append('/usr/lib/python2.7/dist-packages')
+from serial import Serial
+# from time import sleep
+
+
+class RfAtmega128(object):
+    """ Communication with rfAtmega128 board
+        with Spectrum Analyzer firmware
+    """
+    def __init__(self):
+        self.IDN = None         # set device to rfAtmega128
+        self.term = '\r\n'
+        self.port = None
+        self.baudrate = None
+        self.rfAtmega = None
+        self.msg = None
+        self.txMsg = None
+
+    def connect(self, port, baudrate, timeout=1):
+        self.port = port
+        self.baudrate = baudrate
+        try:
+            self.rfAtmega = Serial(self.port, self.baudrate, timeout=1)
+            self.rfAtmega.write('wa' + self.term)
+            # flush the buffer of ATmega128rfa1
+            # otherwise can't start reading the data
+            self.msg = self.rfAtmega.readline().strip()
+            self.msg = self.rfAtmega.readline().strip()
+            self.msg = self.rfAtmega.readline().strip()
+            self.msg = self.rfAtmega.readline().strip()
+            self.IDN = "rfAtmega128"
+            return True
+        except:
+            print 'Error while connecting to rfAtmega128 board.'
+
+    def disconnect(self):
+        if self.rfAtmega is not None:
+            try:
+                self.rfAtmega.close()
+                return True
+            except:
+                print 'Error while disconnecting.'
+        else:
+            print 'Already disconnected.'
+
+    def readRSSI(self):
+        if self.rfAtmega is not None:
+            self.rfAtmega.write("w\r\n")
+            # RX firmware updated only when 'M' transmitted
+            self.rfAtmega.write("M\r\n")
+            # self.msg = self.rfAtmega.readline().strip()
+            # self.msg = self.rfAtmega.readline().strip()
+            self.msg = self.rfAtmega.readline().strip()
+        else:
+            print 'Error receiving RSSI.'
+
+    def clearRSSIbuffer(self):
+        if self.rfAtmega is not None:
+            self.msg = None
+
+    def cwTransmissionStart(self):
+        if self.rfAtmega is not None:
+            # start CW transmission
+            self.rfAtmega.write('F' + self.term)
+            self.txMsg = self.rfAtmega.readline().strip()
+            self.txMsg = self.rfAtmega.readline().strip()
+            self.txMsg = self.rfAtmega.readline().strip()
+            self.txMsg = self.rfAtmega.readline().strip()
+            if 'CW started.' not in self.txMsg and self.txMsg is not '':
+                print 'Error self.txMsg is: ' + self.txMsg
+            self.txMsg = None
+        else:
+            print 'Error CWTX start.'
+
+    def cwTransmissionStop(self):
+        if self.rfAtmega is not None:
+            # stop CW transmission
+            self.rfAtmega.write('S' + self.term)
+            self.txMsg = self.rfAtmega.readline().strip()
+            if 'CW stoped.' not in self.txMsg and self.txMsg is not '':
+                print 'Error self.txMsg is: ' + self.txMsg
+            self.txMsg = None
+        else:
+            print 'Error CWTX stop.'
+
+    def cwTransmissionInit(self):
+        if self.rfAtmega is not None:
+            # stop CW transmission
+            self.cwTransmissionStart()
+            self.cwTransmissionStop()
+            self.cwTransmissionStart()
+        else:
+            print 'Error CWTX init.'
+
+
+def main():
+    cw = RfAtmega128()
+    cw.connect('/dev/ttyUSB1', 9600)
+    cw.cwTransmissionInit()
+    rfAtmega = RfAtmega128()
+    rfAtmega.connect('/dev/ttyUSB0', 9600)
+    rfAtmega.readRSSI()
+    print rfAtmega.msg
+    rfAtmega.readRSSI()
+    print rfAtmega.msg
+    rfAtmega.readRSSI()
+    print rfAtmega.msg
+    rfAtmega.readRSSI()
+    print rfAtmega.msg
+    rfAtmega.disconnect()
+    cw.cwTransmissionStop()
+    cw.disconnect()
+
+if __name__ == "__main__":
+    main()
