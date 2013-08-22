d4617963480c286e7f8fa4844a89cff7e369ef5a
Radoslav Borisov <radoslav.borisov@gmail.com>
2013-08-22 10:24:55 +0300

    Update config file to have reprap configuration inside. Prettify the .xml file.

--- em3dterminal/createConfigXML.py
+++ em3dterminal/createConfigXML.py
@@ -1,34 +1,52 @@
 import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
 
 
 def main():
     # create main component
     root = ET.Element("config")
 
+    # create reprap configuration
+    reprap = ET.SubElement(root, "reprap")
+    reprap.set("firmware", "Sprinter")
+    rr_port = ET.SubElement(reprap, "port")
+    rr_port.text = "/dev/ttyACM0"
+    rr_baud = ET.SubElement(reprap, "baud")
+    rr_baud.text = "115200"
+
     # create pna configuration
     pna = ET.SubElement(root, "pna")
     pna.set("name", "N5230C")
-    ip = ET.SubElement(pna, "ip")
-    ip.text = "10.1.15.106"
-    port = ET.SubElement(pna, "port")
-    port.text = "5024"
-    calib = ET.SubElement(pna, "calib")
-    calib.text = "calibrationRado1.csa"
+    pna_ip = ET.SubElement(pna, "ip")
+    pna_ip.text = "10.1.15.106"
+    pna_port = ET.SubElement(pna, "port")
+    pna_port.text = "5024"
+    pna_calib = ET.SubElement(pna, "calib")
+    pna_calib.text = "calibrationRado1.csa"
 
     # create atmega configuration
     atmega = ET.SubElement(root, "atmega")
-    baud = ET.SubElement(atmega, "port")
-    baud.text = "/dev/ttyUSB0"
     atmega.set("name", "atmega128rfa1")
-    baud = ET.SubElement(atmega, "baud")
-    baud.text = "9600"
+    atmega_baud = ET.SubElement(atmega, "port")
+    atmega_baud.text = "/dev/ttyUSB0"
+    atmega_baud = ET.SubElement(atmega, "baud")
+    atmega_baud.text = "9600"
 
-    tree = ET.ElementTree(root)
-    tree.write("em3da.xml")
+    # create prettified document string
+    document = prettify(root)
 
-    f = open("em3da.xml", 'a')
-    f.write("\n")
-    f.close()
+    # write file to disk
+    output_file = open('em3da.xml', 'w')
+    output_file.write(document)
+    output_file.close()
 
     print "Config file successfully generated."
 
